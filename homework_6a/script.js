de678f4477e7528a39025561ae55f8d46cf114d5
mtsang1 <60626451+mtsang1@users.noreply.github.com>
2020-03-02 14:37:19 -0500

    started on homework 6a

---
+++ homework_6a/script.js
@@ -0,0 +1,111 @@
+
+      var container = document.getElementById("productdetails.view0");
+      function change_img(image) {
+        container.src = image.src;
+      }
+
+      var countb1 = 1;
+          function setColor() {
+              var property = document.getElementById("gbutton");
+              if (countb1 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb1 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb1 = 0;
+              }
+          }
+
+      var countb2 = 1;
+        function setColor2() {
+              var property = document.getElementById("gbutton2");
+              if (countb2 == 0) {
+                property.style.backgroundColor = "#FFF"
+                countb2 = 1;        
+            }
+            else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb2 = 0;
+              }
+          }
+
+      var countb3 = 1;
+        function setColor3() {
+              var property = document.getElementById("gbutton3");
+              if (countb3 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb3 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb3 = 0;
+              }
+          }
+
+      var countb4 = 1;
+        function setColor4() {
+              var property = document.getElementById("gbutton4");
+              if (countb4 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb2 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb4 = 0;
+              }
+          }
+ 
+    
+   
+      var countb1 = 1;
+          function setColor5() {
+              var property = document.getElementById("qbutton");
+              if (countb1 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb1 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb1 = 0;
+              }
+          }
+
+      var countb2 = 1;
+        function setColor6() {
+              var property = document.getElementById("qbutton2");
+              if (countb2 == 0) {
+                property.style.backgroundColor = "#FFF"
+                countb2 = 1;        
+            }
+            else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb2 = 0;
+              }
+          }
+
+      var countb3 = 1;
+        function setColor7() {
+              var property = document.getElementById("qbutton3");
+              if (countb3 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb3 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb3 = 0;
+              }
+          }
+
+      var countb4 = 1;
+        function setColor8() {
+              var property = document.getElementById("qbutton4");
+              if (countb4 == 0) {
+                  property.style.backgroundColor = "#FFF"
+                  countb2 = 1;        
+              }
+              else {
+                  property.style.backgroundColor = "#18A0FB"
+                  countb4 = 0;
+              }
+          }
