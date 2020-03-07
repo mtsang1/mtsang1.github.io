7a1d9f4802f2f5f9e3b34b3abe53889aa388410d
mtsang1 <60626451+mtsang1@users.noreply.github.com>
2020-03-06 20:27:38 -0500

    created a wishlist and made edits to rest of the files per validator request

--- assgn6/script.js
+++ assgn6/script.js
@@ -1,9 +1,7 @@
 
 
-
         // calculates total cost on product details page
-       function checkTotal() {
-
+          function checkTotal() {
           var quantity = document.querySelector('input[name="quantity"]:checked').value;
           total=0;
 
@@ -22,12 +20,8 @@
           if (quantity == 3) {
             total = 21.70;
           }
-
-
-          document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2);
-       
+          document.getElementById("total").innerHTML = "Total: $" + total.toFixed(2);      
         }
-
         // changes the glazing pictures based on option
         function changeGlazingPicture(){ 
             var current = document.getElementsByClassName('active');
@@ -42,7 +36,6 @@
           var container = document.getElementById("none");
           container.src = image.src;
           }
-
         // modal popup box
         window.onload=function(){
         var modal = document.querySelector(".modal");
@@ -58,118 +51,9 @@
             toggleModal();
           }
         }
-
         addToCartButton.addEventListener("click", toggleModal);
         closeButton.addEventListener("click", toggleModal);
         window.addEventListener("click", windowOnClick);
       }
 
 
-/**
-      var countb1 = 1;
-          function setColor() {
-              var property = document.getElementById("gbutton");
-              if (countb1 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb1 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb1 = 0;
-              }
-          }
-
-      var countb2 = 1;
-        function setColor2() {
-              var property = document.getElementById("gbutton2");
-              if (countb2 == 0) {
-                property.style.backgroundColor = "#FFF"
-                countb2 = 1;        
-            }
-            else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb2 = 0;
-              }
-          }
-
-      var countb3 = 1;
-        function setColor3() {
-              var property = document.getElementById("gbutton3");
-              if (countb3 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb3 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb3 = 0;
-              }
-          }
-
-      var countb4 = 1;
-        function setColor4() {
-              var property = document.getElementById("gbutton4");
-              if (countb4 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb2 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb4 = 0;
-              }
-          }
- 
-    
-   
-      var countb1 = 1;
-          function setColor5() {
-              var property = document.getElementById("qbutton");
-              if (countb1 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb1 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb1 = 0;
-              }
-          }
-
-      var countb2 = 1;
-        function setColor6() {
-              var property = document.getElementById("qbutton2");
-              if (countb2 == 0) {
-                property.style.backgroundColor = "#FFF"
-                countb2 = 1;        
-            }
-            else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb2 = 0;
-              }
-          }
-
-      var countb3 = 1;
-        function setColor7() {
-              var property = document.getElementById("qbutton3");
-              if (countb3 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb3 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb3 = 0;
-              }
-          }
-
-      var countb4 = 1;
-        function setColor8() {
-              var property = document.getElementById("qbutton4");
-              if (countb4 == 0) {
-                  property.style.backgroundColor = "#FFF"
-                  countb2 = 1;        
-              }
-              else {
-                  property.style.backgroundColor = "#18A0FB"
-                  countb4 = 0;
-              }
-          }
-
-      **/ 
