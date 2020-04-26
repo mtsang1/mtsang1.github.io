56374b30cf19d88541abb99e2eb8f4bd485bbe54
mtsang1 <60626451+mtsang1@users.noreply.github.com>
2020-04-26 17:51:29 -0400

    Tried to link the pages together

--- Final_Project/Assets/script.js
+++ Final_Project/Assets/script.js
@@ -15,10 +15,3 @@ $(function() {
 });
 
 
-  $(document).ready(function() {
-    $('.carousel .carousel-caption').css('zoom', $('.carousel').width()/1250);
-  });
-
-  $(window).resize(function() {
-    $('.carousel .carousel-caption').css('zoom', $('.carousel').width()/1250);
-  });
